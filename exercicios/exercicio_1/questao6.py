cb91e14983169b4227650f1e1292b577634ff95a
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-13 01:01:54 -0300

    Update questao6.py

---
+++ exercicios/exercicio_1/questao6.py
@@ -0,0 +1,8 @@
+#6 Realiza a leitura de 1 float referente ao salário do 
+#cidadão e apresenta o salário com reajuste de 10% da inflação.
+
+value=float(input('digite o seu salario aqui: '))
+
+wage=(value*0.1)+value
+
+print('seu salario foi reajustado para:',wage)
