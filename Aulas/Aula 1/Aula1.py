958778c5f68f1efdc26a0cae5bdb1f0d12506282
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-13 19:50:38 -0300

    Create Aula1.py

---
+++ Aulas/Aula 1/Aula1.py
@@ -0,0 +1,15 @@
+print('olá mundo')
+print(1)
+eggs=15
+bacon=10
+#vai ler a variavel eggs e a variavel bacon
+print('nós temos',eggs, 'ovos e',bacon,'kg de bacon' )
+ano= 1985
+nome= 'João Matheus'
+salario= 10000.50
+PossuiEmprego= True
+#vai mostrar a classe
+print(type(ano))
+print(type(nome))
+print(type(salario))
+print(type(PossuiEmprego))
