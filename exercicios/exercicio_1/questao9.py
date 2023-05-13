a005aea9c7c4c7af84204fc0ddc46b7ef2b2a06a
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-13 01:23:43 -0300

    Create questao9.py

---
+++ exercicios/exercicio_1/questao9.py
@@ -0,0 +1,9 @@
+#9 Faça um Programa que leia dois números inteiros e mostre o maior deles
+
+number1=int(input("Digite o primeiro numero aqui: "))
+number2=int(input("Digite o segundo numero aqui: "))
+
+if number1 > number2:
+    print('O maior numero é',number1)
+else:
+    print('O maior numero é',number2)
