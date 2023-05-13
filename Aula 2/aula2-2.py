c534e94bad4365f1502d4339687aef382b305094
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-13 02:24:56 -0300

    Update aula2-2.py

---
+++ Aula 2/aula2-2.py
@@ -0,0 +1,10 @@
+membro1= 3
+membro2= 2
+
+print(membro1+membro2)
+print(membro1-membro2)
+print(membro1*membro2)
+print(membro1/membro2)
+print(membro1**membro2)
+print(membro1%membro2)
+print(membro1//membro2)
