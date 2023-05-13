e429dfef264b87264dfc171340bdda8343beea00
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-13 01:32:20 -0300

    Create questao11.py

---
+++ exercicios/exercicio_1/questao11.py
@@ -0,0 +1,28 @@
+#11 As Organizações Hamurabi Medeiros resolveram dar um aumento de salário aos seus colaboradores 
+#e lhe contrataram para desenvolver o programa que calculará os reajustes.
+
+
+
+
+'''
+• Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no
+salário atual:
+• salários até R$ 280,00 (incluindo) : aumento de 20%
+• salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+• salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+• salários de R$ 1500,00 em diante : aumento de 5%
+• Após o aumento ser realizado, informe na tela:
+• o salário antes do reajuste;
+• o percentual de aumento aplicado;
+• o valor do aumento;
+• o novo salário, após o aumento
+'''
+
+'''
+Média Conceito
+Entre 9 e 10 A
+Entre 7.5 e 9 B
+Entre 6 e 7.5 C
+Entre 4 e 6 D
+Entre 0 e 4 E
+'''
