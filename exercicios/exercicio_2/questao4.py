5a5b05c780733d13ae035aaa60aecd949a2cb3d6
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-14 00:07:31 -0300

    Create questao4.py

---
+++ exercicios/exercicio_2/questao4.py
@@ -0,0 +1,13 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+grade1= float(input('digite sua primeira nota: '))
+
+grade2= float(input('digite sua segunda nota: '))
+
+grade3= float(input('digite sua terceira nota: '))
+
+grade4= float(input('digite sua quarta nota: '))
+
+average= (grade1+grade2+grade3+grade4)/4
+
+print('sua media é:',average)
