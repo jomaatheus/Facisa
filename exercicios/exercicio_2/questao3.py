0d16f94642474023244b743a1c713d7359595acc
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-14 00:07:33 -0300

    Create questao3.py

---
+++ exercicios/exercicio_2/questao3.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+number1=int(input('Digite o primeiro numero: '))
+
+number2=int(input('Digite o segundo numero: '))
+
+soma= number1+number2
+
+print(soma)
