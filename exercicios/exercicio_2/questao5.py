3e0c2cf7987d55ad86bfc7f0488db7be340e1b7e
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-14 02:18:34 -0300

    Update questao5.py

--- exercicios/exercicio_2/questao5.py
+++ exercicios/exercicio_2/questao5.py
@@ -1,1 +1,7 @@
 #Faça um Programa que converta metros para centímetros.
+
+metros= int(input('digite o numero em metros: '))
+
+centimetros= metros*100
+
+print('o valor do numeros em centimetros é',centimetros,"cm")
