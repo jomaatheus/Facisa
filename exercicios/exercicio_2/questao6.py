f9935eb524d4940fcaa45d8c7e0bbc6216e4ac23
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-16 22:03:16 -0300

    Update questao6.py

--- exercicios/exercicio_2/questao6.py
+++ exercicios/exercicio_2/questao6.py
@@ -1,1 +1,8 @@
 #• Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+import math
+
+raio=float(input('digite o raio de um circulo: '))
+
+Area=math.pi*raio**2
+
+print('A area do circulo é:',Area)
