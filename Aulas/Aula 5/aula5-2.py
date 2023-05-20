5c70a1c345fa3ecf7f0e754c4eaf329b334eda47
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-19 23:40:39 -0300

    Create aula5-2.py

---
+++ Aulas/Aula 5/aula5-2.py
@@ -0,0 +1,13 @@
+#exemplo 2
+
+filmes = [ ]
+
+#adicionar o elemento
+filmes.append('f1')
+
+#adicionar lendo o teclado
+novoFilme = input('digite o nome do filme ')
+filmes.append(novoFilme)
+
+#caio`s suggestions! not from me!
+filmes.append(input('digite o nome do filme '))
