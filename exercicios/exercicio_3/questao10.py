3a36034870fb1f5a568902486fe5e322388d036b
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-20 19:26:35 -0300

    Update questao10.py

--- exercicios/exercicio_3/questao10.py
+++ exercicios/exercicio_3/questao10.py
@@ -1,2 +1,11 @@
 #Faça um Programa que leia um número e exiba o dia correspondente da semana. 
 #(1-Domingo, 2-Segunda, etc.), se digitar outro valor deve aparecer valor inválido
+
+semana=['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sabado']
+numero=int(input('Digite um numero de 1 a 7: '))
+
+if numero >= 1 and numero <= 7:
+    diaSemana = semana[numero - 1]
+    print ('O dia correspondente ao número', numero , 'é' , diaSemana,'.')
+else:
+    print('Valor inválido. O número deve ser de 1 a 7.')
