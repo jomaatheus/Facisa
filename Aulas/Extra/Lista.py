5ee2608561689fee9fc74cd80c154e05943e67cc
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-20 16:11:59 -0300

    Create Lista.py

---
+++ Aulas/Extra/Lista.py
@@ -0,0 +1,13 @@
+feira=[]
+#lista vazia
+
+feira=['uva','caju','kiwi']
+
+feira.append('banana')
+#apend adiciona um item ao final da lista
+
+print(feira[0])
+#le o primeiro item da lista
+
+print(len(feira))
+#len tamanho da lista
