a75487afd29eb9edfd86a193c68c3b1241da6779
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-20 16:12:01 -0300

    Create while.py

---
+++ Aulas/Extra/while.py
@@ -0,0 +1,13 @@
+opcao=5
+
+while opcao != 4:
+    print('opção 1) listar os filmes')
+    print('opção 2) devolver um filme')
+    print('opção 3) alugar um filme')
+    print('opção 4) sair')
+
+    opcao=int(input('digite uma opção: '))
+    print('opção',opcao,'lida')
+
+
+#WHILE executa uma determinada quantidade de codigo enquanto uma condição for verdadeira.
