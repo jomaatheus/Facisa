b4196a7fb125241fbfa702507cf2c99cb47581e9
matheusfigueiredot <117495280+jomaatheus@users.noreply.github.com>
2023-05-24 07:46:27 -0300

    Create aula7-3.py

---
+++ Aulas/Aula 7/aula7-3.py
@@ -0,0 +1,14 @@
+'''
+O funcionamento do FOR é
+apresentado à direita. Para cada item
+da sequencia (frutas), antes de
+executar os comandos, verifica-se se é
+o último elemento da sequencia. Caso
+não seja, executa-se o corpo do for
+(linhas 3,4 e 5).
+• Quandoalcançarofimdalista,pulao
+corpodoforesegueofuncionamento
+doprograma(linha6eseguintes)
+• Verifiqueocódigoaseguir
+
+'''
