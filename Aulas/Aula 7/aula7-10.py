9045d548b4fd74ac4a0f45c58f74c3069c227a12
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2023-05-30 05:45:24 -0300

    Create aula7-10.py

---
+++ Aulas/Aula 7/aula7-10.py
@@ -0,0 +1,14 @@
+'''
+Exemplo #2: Este se refere a um dos usos comuns do WHILE,
+que é de apresentar menus de opções ao usuário. No exemplo a
+seguir, ele vai apresentar as opções 1) Saque 2) Extrato 3) Sair
+enquanto o usuário não digitar 3, que é Sair da aplicação.
+
+'''
+
+print('Bem-vindo ao banco')
+
+command='0'
+while command != '3':
+    print('1) Saque 2) Extrato 3) Sair')
+    command=input('digite sua opção: ')
