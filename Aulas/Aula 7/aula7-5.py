c8268ca5a1e6755564a3e40426c4c4a3923e7777
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2023-05-30 05:22:56 -0300

    Create aula7-5.py

---
+++ Aulas/Aula 7/aula7-5.py
@@ -0,0 +1,24 @@
+'''
+• Exemplo #1: Na linha 1, declaramos uma lista com 4 elementos,
+enquanto na linha seguinte, declaramos uma variável soma, que vai
+armazenar a soma de todos os elementos de uma lista.
+'''
+
+numeros=[6,5,3,8]
+
+soma=0
+
+for num in numeros:
+    soma= soma + num
+
+print(' a soma é,',soma)
+
+'''
+Na linha 4, iniciamos o for e para cada um dos números da lista,
+executamos a linha soma = soma+numque basicamente:
+• Pegaovalorquetememsomaeacrescea um número da
+lista
+• Aotérminodofor,avariávelsomateráovalordasomade
+todasasvariáveis.
+
+'''
