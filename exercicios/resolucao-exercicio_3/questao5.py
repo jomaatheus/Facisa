1844c545d458e373fb554f703a23efa21956b12e
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2023-05-30 05:12:09 -0300

    Create questao5.py

---
+++ exercicios/resolucao-exercicio_3/questao5.py
@@ -0,0 +1,13 @@
+#Faça um Programa que leia três números e mostre o maior deles
+
+#numeros = [ numero1, numero2, numero3 ]
+numeros = [ ]
+numeros.append(8)
+numeros.append(4)
+numeros.append(3)
+numeros.append(120)
+
+menor = min(numeros)
+maior = max(numeros)
+print('menor eh',menor,'e o maior eh',maior)
+
