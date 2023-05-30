7c2e39afbf1b2e1a05e9f983baba1f252c39ba49
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2023-05-30 05:12:01 -0300

    Create aula7-4.py

---
+++ Aulas/Aula 7/aula7-4.py
@@ -0,0 +1,8 @@
+'''
+Em um FOR é possível (mas não muito comum) termos um
+ELSE, como exemplificado na imagem a seguir. Quando
+terminarmos o FOR das linhas 2 a 5, o ELSE da linha 6 (e
+consequentemente a linha 7) serão executados.
+• Ao término da execução da linha 7, a linha 8 é executada.
+
+'''
