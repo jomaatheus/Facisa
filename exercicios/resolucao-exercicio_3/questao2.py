ef500724a74b14edb61da6d5a42f3add45f6890e
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2023-05-30 05:12:07 -0300

    Create questao2.py

---
+++ exercicios/resolucao-exercicio_3/questao2.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo
+
+numero1 = int(input('digite o valor '))
+
+if numero1 >= 0:
+    print('positivo')
+else:
+    print('negativo')
