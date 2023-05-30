69f24eaf76561c2c4fc17732915bc59d00f55459
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2023-05-30 05:11:57 -0300

    Update aula7-3.py

--- Aulas/Aula 7/aula7-3.py
+++ Aulas/Aula 7/aula7-3.py
@@ -6,9 +6,18 @@ executar os comandos, verifica-se se é
 o último elemento da sequencia. Caso
 não seja, executa-se o corpo do for
 (linhas 3,4 e 5).
-• Quandoalcançarofimdalista,pulao
-corpodoforesegueofuncionamento
-doprograma(linha6eseguintes)
-• Verifiqueocódigoaseguir
+• Quando alcançar o fim da lista, pula o
+corpo do for e segue o funcionamento
+do programa(linha 6 e seguintes)
+• Verifique o código a seguir
 
 '''
+
+for n in range (0,5,2):
+    print(n)
+
+for n in range (0,5):
+    print(n)
+
+for n in range (5):
+    print(n)
