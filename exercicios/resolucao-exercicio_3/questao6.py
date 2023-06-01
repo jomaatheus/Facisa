396e9bf3d65a9241a497f0a579140715df639b17
João Matheus <117495280+jomaatheus@users.noreply.github.com>
2023-05-31 22:56:11 -0300

    Update questao6.py

--- exercicios/resolucao-exercicio_3/questao6.py
+++ exercicios/resolucao-exercicio_3/questao6.py
@@ -1,10 +1,10 @@
 #usando sum e len together
-
+'''
 notas = [ 7, 7, 7, 10 ]
 media = sum(notas) / len(notas)
 media = round(media,2)
 print(media)
-
+'''
 # fazendo medias CODIGO PANCADA!!!!!!!!!! Nao chore, Caio!
 
 nota = 0
